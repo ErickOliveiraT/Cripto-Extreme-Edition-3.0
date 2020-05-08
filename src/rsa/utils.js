c84418b84cf02110ef14357616e85de09f4b7513
Erick <erick.teixeira_50@hotmail.com>
2020-05-08 10:13:45 -0300

    rsa

---
+++ src/rsa/utils.js
@@ -0,0 +1,20 @@
+const NodeRSA = require('node-rsa');
+
+function getKeys(bits) {
+    return new NodeRSA({b: bits});
+}
+
+function encrypt(plain, key) {
+    return key.encrypt(plain, 'base64');
+}
+
+function decrypt(enc, key) {
+    return key.decrypt(enc, 'utf8');
+}
+
+/* const key = getKeys(1024);
+const ent = 'lorem ipsum';
+const enc = encrypt(ent,key);
+console.log(enc);
+const dec = decrypt(enc,key);
+console.log(dec); */
